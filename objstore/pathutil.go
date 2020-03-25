ea5d05888ef314668125ee2616c8440b23a7ccbf
J. David Lee <johnl@crumpington.com>
2020-03-25 11:33:33 +0100

    Added pathutils and tests.

--- objstore/pathutil.go
+++ objstore/pathutil.go
@@ -5,3 +5,19 @@ import "strings"
 func Join(parts ...string) string {
 	return strings.Join(parts, "/")
 }
+
+func Base(path string) string {
+	idx := strings.LastIndex(path, "/")
+	if idx == -1 {
+		return path
+	}
+	return path[idx+1:]
+}
+
+func Dir(path string) string {
+	idx := strings.LastIndex(path, "/")
+	if idx == -1 {
+		return ""
+	}
+	return path[:idx]
+}
---
+++ objstore/pathutil_test.go
@@ -0,0 +1,61 @@
+package objstore
+
+import "testing"
+
+func TestBase(t *testing.T) {
+	type TestCase struct {
+		In, Out string
+	}
+
+	cases := []TestCase{
+		{
+			In:  "",
+			Out: "",
+		}, {
+			In:  "/",
+			Out: "",
+		}, {
+			In:  "xyz",
+			Out: "xyz",
+		}, {
+			In:  "abcd/abc",
+			Out: "abc",
+		},
+	}
+
+	for _, tc := range cases {
+		out := Base(tc.In)
+		if out != tc.Out {
+			t.Fatalf("%v != %v", out, tc.Out)
+		}
+	}
+}
+
+func TestDir(t *testing.T) {
+	type TestCase struct {
+		In, Out string
+	}
+
+	cases := []TestCase{
+		{
+			In:  "",
+			Out: "",
+		}, {
+			In:  "/",
+			Out: "",
+		}, {
+			In:  "xyz",
+			Out: "",
+		}, {
+			In:  "abcd/abc",
+			Out: "abcd",
+		},
+	}
+
+	for _, tc := range cases {
+		out := Dir(tc.In)
+		if out != tc.Out {
+			t.Fatalf("%v != %v", out, tc.Out)
+		}
+	}
+}
